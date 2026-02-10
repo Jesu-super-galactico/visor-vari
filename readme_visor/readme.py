e9eab501d76882e27075e5b97aa1561057c6e697
Jesu-super-galactico <from.colombia.to.all@gmail.com>
2026-02-10 18:28:27 -0500

    depuración

--- readme_visor/readme.py
+++ readme_visor/readme.py
@@ -2,15 +2,15 @@
 print("\
 \n\
 Para que pueda emplear la libreria visor_vari \n\
-usted deberá traer la clase Super_tabla del \n\
-modulo 'variables_valores' que se encuentra en \n\
-la ruta: visor_vari.see \n\
-o en su defecto traerse el objeto 'refer' de ese \n\
+usted deberá traer la clase 'Super_tabla' del \n\
+modulo 'see' que se encuentra en \n\
+la ruta: visor_vari.mas_bajo_nivel \n\
+o en su defecto traerse el objeto 'refer_0' de ese \n\
 modulo, que es un objeto ya formado alli. \n\
 \n\
 Con ese objeto usted podra guardar su variable \n\
 en cualquier instancia. A usted, la libreria le \n\
-proporciona (81) instancias para guardar diferentes \n\
+proporciona (81) instancias, de refer_0, para guardar diferentes \n\
 valores. \n\
 Comprendida desde 'self.selda_0' \n\
 hasta 'self.selda_80' \n\
@@ -19,92 +19,113 @@ Sirvase usar la que usted prefiera. \n\
 Cuando quiera ver sus datos recogidos \n\
 (en cierto punto de la ejecucion de su programa) \n\
 deberá hacer la llamada a la funcion 'gentil' \n\
-que se encuentra en el modulo 'visorquipus' \n\
+que se encuentra en el modulo 'viendo' \n\
 del mismo paquete 'visor_vari'. \n\
 Por supuesto, tambien deberá hacer \n\
 esta importacion antes, en su propio modulo. \n\
 \n\
-La funcion 'gentil' no recibe agumentos. \n\
-en su forma de empleo mas simple. \n\
-visor vari tiene ocho formas de trabajar \n\
-de las cuales cuatro no usan ventana \n\
-y cuatro si. \n\
+visor vari tiene tres formas de trabajar \n\
+de las cuales dos necesitan una predefinicion \n\
+llamadas 'en_cadena' y 'faseypulso' \n\
+esta ultima faseypulso requiere del empleo del metodo 'ultimate'\n\
+despues de todos los gentiles.\n\
+Pero, los detalles de estas formas de trabajo se describen en el readme_tipos\n\
+\n\
+La primera forma de trabajo (la simple) en \n\
+la funcion 'gentil' puede no recibir agumentos. \n\
 \n\
 ''' Ejemplo de ejecución en su forma mas simple ''' \n\
     (no recibe atributos de entrada) \n\
 \n\
-from visor_vari.see import refer \n\
-from visor_vari.visorquipus import gentil \n\
+from visor_vari import gentil \n\
+from visor_vari.mas_bajo_nivel.see import refer_0 \n\
 \n\
 a= 10 \n\
 b= 15 \n\
 \n\
-refer.selda_0= a \n\
-refer.selda_1= b \n\
+refer_0.selda_0= a \n\
+refer_0.selda_1= b \n\
 \n\
 gentil() \n\
 \n\
-refer.selda_0= 43   # Se modifica el valor internamente \n\
+refer_0.selda_0= 43   # Se modifica el valor internamente \n\
                     # quizas al presionarse un boton. \n\
 \n\
 c= 17               # Se hacen presente otros valores \n\
-refer.selda_41= c   # y... tambien se quieren ver. \n\
+refer_0.selda_41= c   # y... tambien se quieren ver. \n\
 \n\
 gentil() \n\
 \n\
-Como podra darse cuenta 'visor_vari' aqui, detecta los \n\
-cambios que puedan darse en 'self.selda_0' que es \n\
-en su programa la variable 'a'. \n\
-En la linea donde se le asigna el valor (43) a esa variable \n\
+Como podra darse cuenta 'visor_vari' aqui, detecta, \n\
+los cambios que puedan darse en 'self.selda_0' \n\
+que es, en el programa, la variable 'a'. \n\
+(En la linea donde se le asigna el valor 43 a esa variable) \n\
 estamos dando a entender \n\
-que la variable podria modificarse (internamente) \n\
-Porque... por ejemplo: \n\
+que la variable podria modificarse (internamente). \n\
+\n\
+Por ejemplo: \n\
 En el trayecto de ejecucion del programa \n\
-se ejecuto una funcion que la modifico, o porque se \n\
-refresco (reseteo la variable) al haber entrado a \n\
-una seccion que hace tal cosa, o porque usted se \n\
-encuentra usando tkinter y quedo la ventana (tk) \n\
-esperando una entrada y usted presiono algun boton \n\
-que ingreso ese valor (43) a la variable. \n\
-yo coloco el 'refer.selda_0= 43' solo para \n\
-indicar el cambio. \n\
-Pero es su programa como tal, el que se encuentra \n\
+se ejecuto una funcion que la modifico (reseteo la variable) \n\
+al haber entrado a una seccion que hace tal cosa. \n\
+O porque se encuentra usando tkinter y tk quedo \n\
+esperando una entrada de valor, \n\
+que usted posteriormente ingreso. \n\
+Yo coloco la linea: 'refer.selda_0= 43' en este codigo de ejemplo, \n\
+solo para indicar el cambio. \n\
+Pero en realidad, es el programa como tal, el que se encuentra \n\
 realizando cambios, \n\
-que usted por supuesto quiere ver con mayor detalle. \n\
-Esos cambios podra verlos posteriormente \n\
-en la segunda llamada a la funcion 'gentil' en este ejemplo. \n\
+los que usted... por supuesto, quiere ver con mayor detalle. \n\
+\n\
+Ademas... agregando posteriores un segundo... 'gentil()' \n\
+como en el ejemplo de arriba, \n\
+podrá ver esos cambios. \n\
+\n\
+En la forma simple de usar visor_vari, en la funcion 'gentil', \n\
+se toman dos argumentos: 1.numero y 2.pausado\n\
+estos dos argumentos son por defecto, cero (0) y True, respectivamente. \n\
+Pausado se podria poner en False, para que no se detenga la ejecucion, \n\
+pero eso sera en versiones posteriores de visor_vari. \n\
+por otro lado, numero= 0, si se recomienda usarlo con cero, ya que no se\n\
+considero otro valor para esta forma simple de visor_vari.\n\
 \n\
 La funcion gentil como usted sabra \n\
 podra llamarla cuantas veces quiera.\n\
 \n\
 Para efectos practicos... donde necesite \n\
-hacer revicion de alguna variable coloque \n\
-'gentil()', pero antes de eso una funcion \n\
-o llamada de funcion, \n\
-que debera crear usted, con el nombre que \n\
-usted mismo quiera. \n\
+hacer revicion de alguna variable, \n\
+en cierto punto de su programa, \n\
+debera colocar 'gentil()'. \n\
+Pero antes de eso, \n\
+cree y llame a una funcion \n\
+(colocandole el nombre que usted mismo quiera) \n\
 Y en ella, haga las \n\
-actualizaciones del objeto/imagen refer. \n\
-Por ejemplo, la de: \n\
-selda_0= 'x' \n\
-selda_1= 'y' \n\
-selda_2= 'z' \n\
+actualizaciones de los objetos/imagenes refer_x. \n\
+Por ejemplo, las de: \n\
+selda_0= 'a' \n\
+selda_1= 'b' \n\
+selda_41= 'c' \n\
 ... \n\
-Esto garantizara \n\
+en este caso, \n\
+\n\
+Con esto garantizara \n\
 que se le este mostrando, \n\
 la configuracion de las variables, \n\
+que usted realmente quiere ver, \n\
 para este momento de la ejecucion. \n\
 \n\
-Pero trabajar asi (con viso_vari) le permite a usted tambien \n\
-que la funcion, la pueda usar en otros puntos... sin tener \n\
-que indicar nuevamente, una por una, las variables \n\
+Pero trabajar asi (con viso_vari) le permite a usted, tambien, \n\
+que esta misma funcion, la pueda usar en otros puntos... \n\
+sin tener que indicar nuevamente, una por una, las variables \n\
 que se quieran ver. \n\
-Evitando asi, apilar tanto codigo en su modulo. \n\
+Inclusive hasta podria, de ser posible, \n\
+crear un modulo aparte donde se encuentren \n\
+todas estas funciones. \n\
+Evitando asi, apilar tanto codigo en su propio modulo. \n\
 \n\
-En el readme que se encuentra junto con este. \n\
-llamado readme_tipos \n\
-(from visor_vari.readme_visor import readme_tipos) \n\
-podra conocer mas a profundidad cada uno \n\
-de las ocho formas de usar visor_vari. \n\
+En el readme que se encuentra adjunto con este y que \n\
+es llamado readme_tipos \n\
+(import visor_vari.readme_visor.readme_tipos) \n\
+podra conocer con mas profundidad \n\
+las otras formas de usar visor_vari. \n\
 ")
 
--- readme_visor/readme_tipos.py
+++ readme_visor/readme_tipos.py
@@ -1,116 +1,144 @@
 
-print("\
-\n\
-Comenzare explicando los cuatro funcionamientos que \n\
-no utilizan ventanas. Estos cuatro tipos tiene cada uno \n\
-su propio configuracion de atributos de entrada, \n\
-que son los que quiero explicar aqui, y son: \n\
-\n\
-ESTE ES VACIO (NO CONTIENE ATRIBUTOS) \n\
-1. gentil( ) \n\
-Funcionamiento: Recorre cada uno de estos \n\
-sin excepcion hasta el final. \n\
-\n\
-CON DOS ATRIBUTOS DE ENTRADAS \n\
-2. gentil( int, str ) \n\
-Funcionamiento: Segun sea el numero del 1er gentil \n\
-que encuentre... todos (del resto) de gentiles tambien \n\
-seran ejecutados. los que tengan un numero diferente \n\
-no seran ejecutados. \n\
-ademas, el segundo atributo puede ser solo una de (2) dos \n\
-posibles palabras, para este caso deberia ser 'serie'. \n\
-Un ejemplo para este caso seria: \n\
-gentil( 1, 'serie' ) \n\
-gentil( 3, 'serie' ) \n\
-gentil( 1, 'serie' ) \n\
-gentil( 1, 'serie' ) \n\
-gentil( 2, 'serie' ) \n\
-Aqui se mostrarian (3) tres visor_varis (los numeros 1) \n\
-pero si al primero se le cambia el 1 por el 2 \n\
-gentil( 2, 'serie' ) \n\
-gentil( 3, 'serie' ) \n\
-gentil( 1, 'serie' ) \n\
-gentil( 1, 'serie' ) \n\
-gentil( 2, 'serie' ) \n\
-se mostrarian (2) dos visor_varis (los numeros 2) \n\
-\n\
-TRABAJA TAMBIEN CON DOS ATRIBUTOS \n\
-3. gentil( int, str ) \n\
-Funcionamiento: Si, este tipo, emplea lo mismo que el \n\
-anterior, de hecho la palabra STR es la misma: 'serie'. \n\
-Lo que hace cambiar el comportamiento de esta, es que, \n\
-antes de, el interprete encontrarse con el 1er gentil debera \n\
-encontrarse con una funcion 'riel( int, int )' que deberas \n\
-importarla desde el modulo visorquipus e ingresarle uno \n\
-o dos enteros. \n\
-Con este entero usted podra ver solo uno o dos \n\
-gentiles, no mas, de todos los que tiene la linea habilitada \n\
-para que se ejecute \n\
-(asi como se explico en el caso 2 que pueden haber varios \n\
-y solo serán ejecutadas... los 5 por ejemplo: gentil( 5, 'serie' ) ) \n\
-Este tipo es muy flexible porque puede pasar del tipo 2 al 3 \n\
-solo predefiniendo riel( ). O del 3 al 2 solo quitandoselo. \n\
-Un ejemplo: \n\
-riel( 0, 1 ) \n\
-gentil( 1, 'serie' ) # Este se mostrará \n\
-gentil( 3, 'serie' ) \n\
-gentil( 1, 'serie' ) # Este se mostrará \n\
-gentil( 1, 'serie' ) # Este, sin embargo, no se mostrará \n\
-gentil( 2, 'serie' ) \n\
-otro... \n\
-riel( 1 ) \n\
-gentil( 1, 'serie' ) # Este, sin embargo, no se mostrará \n\
-gentil( 3, 'serie' ) \n\
-gentil( 1, 'serie' ) # Este se mostrará \n\
-gentil( 1, 'serie' ) # Este, sin embargo, no se mostrará \n\
-gentil( 2, 'serie' ) \n\
-\n\
-REQUIERE DOS ATRIBUTOS DE ENTRADA \n\
-4. gentil( int, str ) \n\
-Funcionamiento: Este tipo o forma de emplear visor_vari es \n\
-parecida a la anterior, pero ocure que, la palabra STR \n\
-del segundo atributo es 'paralelo'. \n\
-Y si, tambien hace uso de riel( ), pero demas... a esto, \n\
-deberas incorporar otra funcion como importacion, \n\
-que se encuentra ubicada en el modulo visorquipus. \n\
-y se llama 'ultimate'. \n\
-La diferencia con todos los demas tipo de funcionamiento, \n\
-es que este, hace funcionar todas las lineas de pulso \n\
-una por una. Ejemplo: \n\
-gentil( 1, 'paralelo' ) \n\
-gentil( 2, 'paralelo' ) \n\
-gentil( 1, 'paralelo' ) \n\
-gentil( 2, 'paralelo' ) \n\
-En este ejemplo, hace sacar primero los dos 1 y \n\
-despues, muestra los valores de los gentiles 2. \n\
-Por supuesto que aqui riel sería riel( 0, 1 ) y asi mostraria \n\
-ambos de cada linea. pero como mencione anteriormente tambien \n\
-puede funcionar con un solo atributo si usted quiere. \n\
-Pero debo mencionar que para este caso o tipo \n\
-de ejecucion se le permitio a riel pasar por atributo \n\
-una lista segun el punto que se quiera ver de cada linea \n\
-y su posible siguiente gentil. \n\
-ejemplo: \n\
-riel( list ) # muestra los puntos indicados en la lista, respectivamente. \n\
-... \n\
-otro ejemplo: \n\
-riel( list, True ) # muestra los puntos y ademas los adyacentes debajo. \n\
-... \n\
-Quitandole el None al segundo atributo, con True, podra ver ademas \n\
-el supuesto siguiente gentil, aparte del que fue señalado en la linea \n\
-en riel (que se encuentre justo debajo). \n\
-Con este tipo de division usted puede visualizar que esta \n\
-pasando en un objeto con los gentiles 1 y que esta \n\
-pasando en los datos o instancias de otro objeto con el \n\
-gentil 2. \n\
-\n\
-PARA EL CASO DE LAS VENTANAS \n\
-(de momentos solo tkinter) \n\
-tienen todas las cuatro tambien el mismo funcionamiento \n\
-sin embargo y por supuesto que la sentencia incluye \n\
-pasar el objeto de la ventana por atributo, esto es asi: \n\
-self.mi_vent= tk.TK( ) \n\
-gentil( int, str, objeto ) \n\
-Es claro que: 'mi_vent' es una instancia de objeto. \n\
-")
+texto_esplicativo= """
+COMO SE DESCRIBE EN EL README (COMUN) DE ESTA LIBRERIA,
+EXISTEN TAN SOLO OTRAS DOS FORMAS ADICIONALES DE EJECUTAR VISOR_VARI,
+QUE SON LAS SIGUIENTES:
+
+1. Predefiniendo anteriormente a los 'gentil'
+una funcion llamada 'en_cadena', la cual recibe tres argumentos:
+  1. un numero
+  2. una lista de indices
+  3. pausado
+  
+En este modo (en_cadena) pausado se define dentro de 'en_cadena'
+como tercer argumento y por defecto es True. En los gentiles no
+se requiere definirlos.
+
+Por otro lado, el primer argumento que se le pasa a 'en_cadena'
+es un numero con el cual son comparados los gentiles...
+y los que, coincidan son ejecutados, los que no, no lo seran.
+
+La lista de indices, hace referencia a cuales de esos 
+"numeros que coinciden" son los que se ejecutaran.
+si la lista no se le pasa como argumento, se ejecutaran todos.
+El primer 'gentil' que coincide tiene el indice 0, el segundo el indice 1
+y asi sucesivamente.
+
+Para esta forma de ejecutar la libreria
+se requiere que 'gentil' exprese su numero.
+Los cuales serviran para compararlos con el numero que se dio, a 'en_cadena'.
+ejemplo: gentil(1)
+
+Un ejemplo mas completo seria el siguiente:
+
+en_cadena(1, [0, 1], pausado=True)
+gentil(1) # Este 'gentil' se ejecutaria, porque su numero coincide con el numero dado a 'en_cadena'.
+gentil(2) # Este 'gentil' no se ejecutaria, porque su numero no coincide con el numero dado a 'en_cadena'.
+gentil(1) # Este 'gentil' se ejecutaria, porque su numero tambien coincide con el numero dado a 'en_cadena'.
+gentil(1) # Este 'gentil' no se ejecutaria, porque en la lista no aparece su indice (2).
+
+
+2. La otra forma de ejecutar la libreria visor_vari es
+predefiniendo anteriormente a los 'gentil'
+una funcion llamada 'faseypulso', la cual recibe tres argumentos:
+  1. bajada (la cual a mi me gusta llamarla profundidad)
+  2. una lista, que es una lista de diccionarios que tienen (en su interior)
+    otra lista... list[ {0: [x]}, {1: [y]}, {2: [z]}...]
+  3. pausado
+  
+En este modo (faseypulso) pausado se define dentro de 'faseypulso'
+como tercer argumento y por defecto es True. En los gentiles no
+se requiere definirlos.
+
+El primer argumento que se llama 'bajada' indica:
+si solo ejecutara los primeros 'gentil' que coincidan 
+o si buscara ejecutar todos (los que coincidan).
+por defecto este argumento es 'False', por los que, por defecto,
+solo se ejecutan los primeros 'gentil' que coincidan.
+
+Aqui quiero detenerme un momento y explicar, cual es la diferncia con el modo 'en_cadena'.
+Esta diferencia es que en el modo 'en_cadena' se considera, solo un pulso,
+algo asi como se ejecuta un modulo de arriba a abajo y ya.
+sin embargo, en el modo 'faseypulso' se consideran varios pulsos, 
+como si se estuviera dentro de un ciclo o bucle.
+podria ser de while o de for.
+
+He aqui otro detalle y es que en este modo (faseypulso) los 'gentil' no se
+comparan con un numero dado (como ocurre en 'en_cadena') sino con la "ola", 
+en la que se encuentran en ese momento los 'gentil' 
+o todo el script.
+
+Para que la libreria identifique cada pulso y le asigne un numero
+se ha establecido que al final del ciclo se cierre con la funcion 'ultimate'.
+Esto garantiza que la libreria sepa 
+que la siguiente vez... que se entre a la libreria, 
+ya se esta en el numero_de_pulso aumentado (aumentado en 1).
+
+A cada pulso se le considera ola y comienza a contar
+desde el pulso cero (0) 
+aunque tengase en cuenta, que, en el titulo de la
+ventana (donde se muestran los valores de las variables)
+se empieza con ola=1, por lo que deberia usted mentalmente descontarle un 1,
+a ese numero que se muestra en la ventana, para saber realmente con
+cual pulso se esta trabajando.
+esta ocurre asi, 
+ya que en 'gentil' se esta considerando desde cero... gentil(0)
+por lo que se ha dejado la diferencia de una (1) unidad, para la ola.
+
+Ademas recuerde que en este modo (faseypulso) se esta comparando con algo...
+y ese algo es la ola.
+Asi que los 'gentil' deben expresar un numero, que indica el numero de ola, 
+a la cual responden.
+
+Si a 'bajada' se le da el valor True, sin 'lista' ni 'pausado', entonces; 
+se ejecutaran todos los 'gentil' que coincidan (segun la ola)
+sin importar su posicion en el script.
+
+La lista pretende actuar similar, que como ocurre, 
+en el modo 'en_cadena'.
+Pero puesto que aqui se esta tratando de pulsos u olas, entiendase como se quiera.
+la lista de este modo (faseypulso) es una lista que contiene diccionarios, 
+cada diccionario tiene una clave/llave que es un numero (0, 1, 2...) y ese numero
+hace referencia a una ola. la 0 hace referencia a la ola 0, la 1 hace referencia a la ola 1 
+y asi sucesivamente. {0: []}, {1: []}, {2: []}
+A su vez, cada uno de estos numeros/indices, tiene una lista interna, 
+que hace referencia a los 'gentil' que se quieren ejecutar para cada ola.
+
+Por ejemplo, si se quieren ejecutar los 'gentil' que responden a la ola 0:
+{0: [del_gentil_0_en_primera_posicion, del_gentil_0_en_tercera_posicion]}
+o lo que es lo mismo: {0: [0, 2]} 
+
+gentil(0) # este 'gentil' se ejecutaria, porque su indice aparece.
+gentil(0) # este 'gentil' no se ejecutaria, porque su indice no aparece..
+gentil(0) # este 'gentil' se ejecutaria, porque su indice aparece.
+
+tenga mucho cuidado a la hora de emplear este modo (faseypulso)
+ya que dentro del modulo o script, pueden aparecer intercalados 'gentil' con
+diferentes numeros (que responden a diferentes olas).
+por lo que si no tiene cuidado se podria confundir.
+Ademas siempre recuerde que cuando es _el_primer_pulso... gentil(0) 
+en la ventana se muestra como ola 1.
+
+Ejemplo para falseypulso:
+faseypulso(False, listando_los_gentiles_que_quiero_ver, pausado=True)
+# ya en bucle.
+gentil(0) # ... y otros mas.
+ultimate() # para que se incremente el numero de ola o pulso.
+
+En esta version de visor_vari (2.0) no se han hecho 
+las funcionalidades para 'pausado' por lo que no tiene porque modificarlo
+si se encuentra en esta version.
+
+Ademas, tenga en cuenta que si la libreria identifica un modo...
+ya no se cambiara a otro metodo.
+esta, en el modo en_cadena cuando a lo primero que entra es a en_cadena(),
+esta, en el modo faseypulso, cuando a lo primero que entra es a faseypulso().
+y si lo primero que encuentra es un gentil()... tambien ese es un modo.
+
+Las diferentes ventanas que van apareciendo, se les ha dispuesto, para que 
+se vallan cambiando de color. esta es una medida para que usted desarrollador se 
+encuentre mas agusto e identifique mas facilmente, cada ventana.
+"""
+
+print(texto_esplicativo)
 
